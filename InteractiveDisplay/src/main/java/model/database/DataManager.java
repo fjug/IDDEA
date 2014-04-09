67f9ffaa8cd9e72074c3fcbfd7673539a9e2bf5c
hkmoon <hkmoon@me.com>
2014-04-09 19:06:30 +0200

    Initial junction and endpoint table.

--- InteractiveDisplay/src/main/java/model/database/DataManager.java
+++ InteractiveDisplay/src/main/java/model/database/DataManager.java
@@ -1,44 +1,75 @@
 package model.database;
 
 import javax.swing.table.DefaultTableModel;
+import java.io.File;
 import java.sql.*;
 
 /**
  * Created by moon on 09/04/14.
  */
 class DataManager {
+    String db = "neurites.db";
     Connection c = null;
     Statement stm=null;
     DataManager(){
         try {
-
+            String workingDirectory = System.getProperty("user.dir");
             Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:sales.db");
+            c = DriverManager.getConnection("jdbc:sqlite:" + db);
             stm=c.createStatement();
-            String sql="CREATE TABLE tblcustomers(Id INT PRIMARY KEY, Name TEXT NOT NULL, Sex TEXT NOT NULL,"+
-                    " Address TEXT NOT NULL, Email TEXT NOT NULL, Phone TEXT NOT NULL)";
-            stm.executeUpdate(sql);
+
+            File f = new File(workingDirectory + "/" + db);
+
+//            if(!f.exists())
+//            {
+//                String sql="CREATE TABLE junctions(Id INT PRIMARY KEY, X INT NOT NULL, Y INT NOT NULL,"+
+//                        " T INT NOT NULL, SpatialNeighbors TEXT, TemporalNeighbors TEXT)";
+//                stm.executeUpdate(sql);
+//
+//                sql="CREATE TABLE endpoints(Id INT PRIMARY KEY, X INT NOT NULL, Y INT NOT NULL,"+
+//                        " T INT NOT NULL, SpatialNeighbors TEXT, TemporalNeighbors TEXT, Junction INT, FOREIGN KEY(Junction) REFERENCES junctions(Id))";
+//                stm.executeUpdate(sql);
+//            }
 
         } catch ( Exception e ) {
             e.printStackTrace();
             System.exit(0);
         }
     }
-    public void insertSampleData(int id,String name, String sex, String address,String email, String phone){
-        String sqlinsertion="INSERT INTO tblcustomers(Id,Name,Sex,Address,Email,Phone) VALUES("+id+",'"
-                +sex+"','"+name+"','"+address+"','"+email+"','"+phone+"')";
+
+    public void insertJunctionData(int id, int x, int y, int t, String spatialN, String temporalN){
+        String sqlinsertion="INSERT INTO junctions(Id, X, Y, T, SpatialNeighbors, TemporalNeighbors) VALUES("+id+","
+                +x+","+y+","+t+",'"+spatialN+"','"+temporalN+"')";
         try{
             stm.executeUpdate(sqlinsertion);
         }catch(SQLException se){se.printStackTrace();}
     }
 
+    public void insertEndpointData(int id, int x, int y, int t, String spatialN, String temporalN, int junction){
+        String sqlinsertion="INSERT INTO endpoints(Id, X, Y, T, SpatialNeighbors, TemporalNeighbors, Junction) VALUES("+id+","
+                +x+","+y+","+t+",'"+spatialN+"','"+temporalN+"'," + junction + ")";
+        try{
+            stm.executeUpdate(sqlinsertion);
+        }catch(SQLException se){se.printStackTrace();}
+    }
+
+
+
     public void getData(DefaultTableModel datamodel){
-        String sqlselection="SELECT * FROM tblcustomers";
+        String sqlselection="SELECT junctions.Id AS Junction, junctions.X AS JX, junctions.Y AS JY, junctions.T AS JT, junctions.SpatialNeighbors AS JSps, junctions.TemporalNeighbors AS JTps, " +
+                "endpoints.Id AS Endpoint, endpoints.X AS EX, endpoints.Y AS EY, endpoints.T AS ET, endpoints.SpatialNeighbors AS ESps, endpoints.TemporalNeighbors AS ETps " +
+                "FROM junctions LEFT JOIN endpoints ON junctions.Id = endpoints.Junction";
         try{
             ResultSet result=stm.executeQuery(sqlselection);
             if(result!=null){
                 while(result.next()){
-                    datamodel.addRow(new Object[]{result.getInt("Id"),result.getString("Name"),result.getString("Sex"),result.getString("Address"),result.getString("Email"),result.getString("Phone")});
+                    datamodel.addRow(new Object[]{
+                            result.getInt("Junction"), result.getInt("JX"), result.getInt("JY"),
+                            result.getInt("JT"), result.getString("JSps"), result.getString("JTps"),
+
+                            result.getInt("Endpoint"), result.getInt("EX"), result.getInt("EY"),
+                            result.getInt("ET"), result.getString("ESps"), result.getString("ETps")
+                    });
                 }
             }
 
